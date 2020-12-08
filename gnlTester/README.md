3c7ca554c53ecaa305d4b9d5e4b7e7e18d79cde4
ckurt <ckurt@student.42lyon.fr>
2020-12-08 10:13:57 +0100

    bonus files

--- gnlTester/README.md
+++ gnlTester/README.md
@@ -1,7 +1,6 @@
-# gnlTester
+# gnlTester (2019+)
 
-Clone this tester in your gnl repository.
-
+Clone this tester in your gnl repository. (works on linux and mac, handles sigsegv on all tests, and timeout on mandatory part)
 ![alt text](https://i.imgur.com/uupv1UH.png)
 
 # Commands
@@ -13,12 +12,15 @@ make vm = launch mandatory tests with valgrind
 make vb = launch bonus tests with valgrind  
 make va = launch mandatory tests + bonus tests with valgrind  
 
+You can change the timeout value in Makefile    
+![alt text](https://i.imgur.com/jUimpaC.png)  
 
 *FOR RIGOROUS TEST YOU SHOULD RUN ALL TESTS WITH VALGRIND*
 
 # Outputs
 
-![alt text](https://i.imgur.com/TghMNXL.png)
+![alt text](https://i.imgur.com/b9hjjWn.png)
+![alt text](https://i.imgur.com/C0ul80f.png)
 
 MOK / MKO = test about your malloc size (this shouldn't be tested by moulinette)  
 
--- main.c
+++ main.c
@@ -6,12 +6,13 @@
 /*   By: ckurt <ckurt@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/29 19:54:28 by ckurt             #+#    #+#             */
-/*   Updated: 2020/12/07 20:34:45 by ckurt            ###   ########lyon.fr   */
+/*   Updated: 2020/12/08 08:51:14 by ckurt            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+#include <fcntl.h>
 
 int main(void)
 {
@@ -30,7 +31,6 @@ int main(void)
 		// free(line);
 	}
 	// printf("return : %d\n line = %s", get_next_line(fd, &line), line);
-	// printf("%d", r);
-	// printf("%s", line);
+	printf("%d", r);
 	return (0);
 }
